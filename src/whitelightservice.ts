b8ddcff7859f1b970feb54439f3b0a9163d9f6bb
Konstantin Kosmatov <key@kosmatov.com>
2022-11-25 09:45:54 +0000

    Remove brightness updates

--- src/whitelightservice.ts
+++ src/whitelightservice.ts
@@ -17,29 +17,10 @@ export class WhiteLightService extends LightService implements ConcreteLightServ
       },
       value => this.sendCommand("set_power", [value ? "on" : "off", "smooth", 500, 0]),
     );
-    this.handleCharacteristic(
-      this.platform.Characteristic.Brightness,
-      async () => {
-        const attributes = await this.attributes();
-        return attributes.bright;
-      },
-      async value => {
-        if (value > 0) {
-          await this.ensurePowerMode(0);
-          await this.sendSuddenCommand("set_bright", value);
-          this.setAttributes({ power: true, bright: value });
-          this.saveDefaultIfNeeded();
-        } else {
-          await this.sendSuddenCommand("set_power", "off");
-          this.setAttributes({ power: false, bright: 0 });
-        }
-      },
-    );
   }
 
   public onAttributesUpdated = (newAttributes: Attributes) => {
     this.debug(`white light updated ${JSON.stringify(newAttributes)}`);
     this.updateCharacteristic(this.platform.Characteristic.On, newAttributes.power);
-    this.updateCharacteristic(this.platform.Characteristic.Brightness, newAttributes.bright);
   };
 }
